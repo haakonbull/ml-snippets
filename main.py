544f370791871b5c2c70450ebc4fe886bd69453a
Håkon Hove <hbhove@gmail.com>
2022-08-11 10:12:14 +0200

    feed forward

--- main.py
+++ main.py
@@ -42,3 +42,9 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
