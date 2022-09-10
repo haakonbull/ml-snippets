d2434f8c1cceef9efd6a155011f5efff558f955c
Håkon Hove <hbhove@gmail.com>
2022-09-10 14:42:45 +0200

    changing back to w

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ class Network(object):
                 activation = sigmoid(z)
                 activations.append(activation)
             # backward pass
-            delta = self.cost_derivative(activations[-1], x) * \
+            delta = self.cost_derivative(activations[-1], y) * \
                 sigmoid_prime(zs[-1])
             nabla_b[-1] = delta
             nabla_w[-1] = np.dot(delta, activations[-2].transpose())
